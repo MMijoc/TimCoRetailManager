6a06aeccc69f622ec97b5fb79b07fec63a1885a9
MMijoc <markomijoc18@gmail.com>
2022-03-03 11:15:21 +0100

    Added tables fot basic register system

---
+++ TRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Product] INT NOT NULL,
+	[Quantity] INT NOT NULL,
+	[PurchasePrice] MONEY NOT NULL,
+	[PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+
+)
