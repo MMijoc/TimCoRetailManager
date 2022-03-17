588a94bdc871e5c52237d05f624f984db6fc2c74
MMijoc <markomijoc18@gmail.com>
2022-03-17 14:31:54 +0100

    Added Transaction
    Added transactions to SQL Data Access and used them for sales

--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace TRMDataManager.Library.Internal.DataAccess
 {
-	internal class SqlDataAccess
+	internal class SqlDataAccess : IDisposable
 	{
 		public string GetConnectionString(string name)
 		{
@@ -37,6 +37,54 @@ namespace TRMDataManager.Library.Internal.DataAccess
 			{
 				connection.Execute(storedProcedure, paramaters, commandType: CommandType.StoredProcedure);
 			}
+		}
+
+
+		private IDbConnection _connection;
+		private IDbTransaction _transaction;
+		public void StartTransaction(string connectionStringName)
+		{
+			string connectionString = GetConnectionString(connectionStringName);
+
+			_connection = new SqlConnection(connectionString);
+			_connection.Open();
+
+			_transaction = _connection.BeginTransaction();
+		}
+
+		public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U paramaters)
+		{
+			List<T> rows = _connection.Query<T>(storedProcedure, paramaters, 
+				commandType: CommandType.StoredProcedure,
+				transaction: _transaction)
+				.ToList();
+
+			return rows;
+		}
+
+		public void SaveDataInTransaction<T>(string storedProcedure, T paramaters)
+		{
+			_connection.Execute(storedProcedure, paramaters, 
+				commandType: CommandType.StoredProcedure,
+				transaction: _transaction);
+		}
+
+		public void CommitTransaction()
+		{
+			_transaction?.Commit();
+			_connection.Close();
+		}
+
+		public void RollbackTransaction()
+		{
+			_transaction?.Rollback();
+			_connection.Close();
+		}
+
+		public void Dispose()
+		{
+			// BUG - connection will already be closed when it comes to this
+			CommitTransaction();
 		}
 	}
 }
