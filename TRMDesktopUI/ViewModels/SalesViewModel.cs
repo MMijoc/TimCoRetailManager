e0c3486f693eec780914ecac2c79d68db6329fff
MMijoc <markomijoc18@gmail.com>
2022-03-21 14:59:00 +0100

    Added Dialog box and permision check
    Checks does user have access and alerts him if he does not have access

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -3,9 +3,11 @@ using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TRMDesktopUI.Library.Api;
 using TRMDesktopUI.Library.Helpers;
 using TRMDesktopUI.Library.Models;
@@ -19,14 +21,18 @@ namespace TRMDesktopUI.ViewModels
 		private readonly IConfigHelper _configHelper;
 		private readonly ISaleEndpoint _saleEndpoint;
 		private readonly IMapper _mapper;
+		private readonly StatusInfoViewModel _status;
+		private readonly IWindowManager _window;
 
 		public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper configHelper,
-			ISaleEndpoint saleEndpoint, IMapper mapper)
+			ISaleEndpoint saleEndpoint, IMapper mapper, StatusInfoViewModel status, IWindowManager window)
 		{
 			_productEndpoint = productEndpoint;
 			_configHelper = configHelper;
 			_saleEndpoint = saleEndpoint;
 			_mapper = mapper;
+			_status = status;
+			_window = window;
 		}
 
 		
@@ -154,7 +160,30 @@ namespace TRMDesktopUI.ViewModels
 		protected override async void OnViewLoaded(object view)
 		{
 			base.OnViewLoaded(view);
-			await LoadProducts();
+			try
+			{
+				await LoadProducts();
+			}
+			catch (Exception ex)
+			{
+				dynamic settings = new ExpandoObject();
+				settings.WindowStrtupLocation = WindowStartupLocation.CenterOwner;
+				settings.ResizeMode = ResizeMode.NoResize;
+				settings.Title = "System Error";
+
+				if (ex.Message == "Unauthorized")
+				{
+					_status.UpdateMessage("Unauthorized Access", "You do not have permission to interact with the Sale Form");
+				}
+				else
+				{
+					_status.UpdateMessage("Fatal Exception", ex.Message);
+
+				}
+
+				await _window.ShowDialogAsync(_status, null, settings);
+				await TryCloseAsync();
+			}
 		}
 		private async Task LoadProducts()
 		{
