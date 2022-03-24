f7ec2d6f9b118505fc366f1199c992420c394f22
MMijoc <markomijoc18@gmail.com>
2022-03-24 14:10:18 +0100

    Added proper connection string

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,18 @@ namespace TRMDataManager.Library.DataAccess
 {
 	public class SaleData
 	{
+		private readonly IConfiguration _config;
+
+		public SaleData(IConfiguration config)
+		{
+			_config = config;
+		}
 		public void SaveSale(SaleModel saleInfo, string casierId)
 		{
 			// TODO: Make this SOLID/DRY/Better
 			// Start filling in the sale detail models we will save to the database
 			List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
-			ProductData products = new ProductData();
+			ProductData products = new ProductData(_config);
 			decimal taxRate = ConfigHelper.GetTaxRate() / 100;
 
 
@@ -54,7 +61,7 @@ namespace TRMDataManager.Library.DataAccess
 
 			sale.Total = sale.SubTotal + sale.Tax;
 
-			using (SqlDataAccess sql = new SqlDataAccess())
+			using (SqlDataAccess sql = new SqlDataAccess(_config))
 			{
 				try
 				{
@@ -87,7 +94,7 @@ namespace TRMDataManager.Library.DataAccess
 
 		public List<SaleReportModel> GetSaleReport()
 		{
-			SqlDataAccess sql = new SqlDataAccess();
+			SqlDataAccess sql = new SqlDataAccess(_config);
 
 			var output = sql.LoadData<SaleReportModel, dynamic>("dbo.spSale_SaleReport", new { }, "TRMData");
 
--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -12,9 +13,19 @@ namespace TRMDataManager.Library.Internal.DataAccess
 {
 	internal class SqlDataAccess : IDisposable
 	{
+		private IDbConnection _connection;
+		private IDbTransaction _transaction;
+		private bool isClosed = false;
+		private readonly IConfiguration _config;
+
+		public SqlDataAccess(IConfiguration config)
+		{
+			_config = config;
+		}
+
 		public string GetConnectionString(string name)
 		{
-			return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+			return _config.GetConnectionString(name);
 		}
 
 		public List<T> LoadData<T, U>(string storedProcedure, U paramaters, string connectionStringName)
@@ -40,9 +51,8 @@ namespace TRMDataManager.Library.Internal.DataAccess
 		}
 
 
-		private IDbConnection _connection;
-		private IDbTransaction _transaction;
-		private bool isClosed = false;
+
+
 		public void StartTransaction(string connectionStringName)
 		{
 			string connectionString = GetConnectionString(connectionStringName);
