feba8186ef686350e46f052991ca5505cc9f5981
MMijoc <markomijoc18@gmail.com>
2022-03-25 13:55:24 +0100

    Added logging for admin actions and moved secure key to appsetings.json

--- TRMApi/Startup.cs
+++ TRMApi/Startup.cs
@@ -61,7 +61,7 @@ namespace TRMApi
 					JwtBearerOption.TokenValidationParameters = new TokenValidationParameters
 					{
 						ValidateIssuerSigningKey = true,
-						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySeacretKeyIsSeacretSoDoNotTell")),
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
 						ValidateIssuer = false,
 						ValidateAudience = false,
 						ValidateLifetime = true,
