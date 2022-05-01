042662c52350ba23b6fdfebfb5ac31a383aa898a
MMijoc <markomijoc18@gmail.com>
2022-05-01 19:54:56 +0200

    Added models for authentication

--- Portal/Models/AuthenticatedUserModel.cs
+++ Portal/Models/AuthenticatedUserModel.cs
@@ -2,5 +2,8 @@
 {
 	public class AuthenticatedUserModel
 	{
+		public string Access_Token { get; set; }
+		public string UserName { get; set; }
+
 	}
 }
--- Portal/Models/AuthenticationUserModel.cs
+++ Portal/Models/AuthenticationUserModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Portal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portal.Models
 {
 	public class AuthenticationUserModel
 	{
+		[Required(ErrorMessage = "Email Address is required")]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "Password is required")]
+		public string Password { get; set; }
+
 	}
 }
