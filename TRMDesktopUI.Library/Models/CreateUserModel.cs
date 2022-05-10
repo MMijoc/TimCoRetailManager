8bf336dcf9f83e035de0c9fe7f358b29fb3056f0
MMijoc <markomijoc18@gmail.com>
2022-05-10 15:07:51 +0200

    Added a page for registrating new users

---
+++ TRMDesktopUI.Library/Models/CreateUserModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TRMDesktopUI.Library.Models
+{
+	public class CreateUserModel
+	{
+		[Required]
+		[DisplayName("First Name")]
+		public string FirstName { get; set; }
+		[Required]
+		[DisplayName("Last Name")]
+		public string LastName { get; set; }
+		[Required]
+		[EmailAddress]
+		[DisplayName("Email Address")]
+		public string EmailAddress { get; set; }
+		[Required]
+		public string Password { get; set; }
+		[Required]
+		[Compare(nameof(Password), ErrorMessage = "The password do not match")]
+		[DisplayName("Confirm Password")]
+		public string ConfirmPassword { get; set; }
+	}
+}
